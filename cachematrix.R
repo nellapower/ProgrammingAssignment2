d9a5ab606e998bff43b65a67f19d2c097430b3bf
nellapower <helena_bukvova@hotmail.com>
2014-04-26 16:24:55 +0200

    created functions that cache matrix inversion

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions make the repetitive computation of matrix inversion
+##more effective, by caching the once calculated matrix inverse. The first
+##function provides the caching functionality, the second itneracts with the
+##cache.
 
-## Write a short comment describing this function
+## This functions initiates a cache of a matrix and its inverse. It returns a list of four functions:
+## - set (caches the matrix)
+## - get (retrives the matrix)
+## - setinv (caches the inverse)
+## - getinv (retrieves the inverse)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL                   #set an empty cache for the inverse
+  set <- function(y) {        #cache the matrix and empty the inverse cache
+       x <<- y                
+       m <<- NULL
+    }
+  get <- function() x                #retrieve the cached matrix
+  setinv <- function(inv) m <<- inv  #cache a calculated inverse
+  getinv <- function() m             #retrieve the cached inverse
+  list(set = set, get = get, setinv = setinv, getinv = getinv) #return list
 }
 
 
-## Write a short comment describing this function
+## This function interacts with the makeCacheMatrix function. It cecks, if a
+## cached version of the matrix inverse exists; if yes it retrieves it, if
+## not it calculates the inverse and caches it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()                   #retrieve the inverse cache
+  if(!is.null(m)) {                 #return the inverse if available 
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()                   #get the matrix
+  m <- solve(data, ...)             #create an inverse
+  x$setinv(m)                       #cache it
+  m                                 #return the inverse
 }
